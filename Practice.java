eccafebe06b3d67b70ba13fc5d364855148aea19
Archit jain <77152776+codie-kid@users.noreply.github.com>
2022-03-22 12:31:12 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,11 @@
+class Practice{
+    public static void main(String args[]) {
+        String type = args[0];
+        String model = args[1];
+        String premiumType = args[2];
+        String costPrice = args[3];
+        for(int i=0;i<=args.length;i++){
+            System.out.println(args[i]);
+        }
+        }
+    }
